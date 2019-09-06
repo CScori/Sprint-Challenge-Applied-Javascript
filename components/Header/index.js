18ca68f2c91ba75c7de4edb838141e905a063bc9
CScori <amilanotte1366@gmail.com>
2019-09-06 10:14:10 -0500

    adds element creation on header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,12 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const head = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    head.classList.add('header')
+    
+}
