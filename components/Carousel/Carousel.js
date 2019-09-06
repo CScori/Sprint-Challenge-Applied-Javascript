f23e6025362fb21dbc69a344e21e63508a6681f4
CScori <amilanotte1366@gmail.com>
2019-09-06 11:50:37 -0500

    adds array of src to iterate through

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -18,7 +18,7 @@
   </div>
 */
 
-
+let imgArray = ['./assets/carousel/mountains.jpeg', './assets/carousel/computer.jpeg', './assets/carousel/trees.jpeg', './assets/carousel/turntable.jpeg']
 
 function CreateCarousel (data) {
   const carousel = document.querySelector('.carousel-container')
